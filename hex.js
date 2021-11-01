f2c4e1dfbd31c97b5865541ab00e68c9cb7aed36
Louie Camacho <louie.camacho@hogarth-ogilvy.com>
2021-11-01 15:34:19 -0500

    copied from app.js

--- hex.js
+++ hex.js
@@ -1,1 +1,17 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const selector = (query) => document.querySelector(query);
+
+const btn = selector('#btn');
+const color = selector('.color');
+
+
+btn.addEventListener('click', () => {
+    // get random number between 0 - 3
+    const randomNumber = getRandomNum(3);
+    console.log(randomNumber);
+    document.body.style.backgroundColor = hex[randomNumber];
+    color.textContent = hex[randomNumber];
+});
+
+let getRandomNum = () => Math.floor(Math.random(3) * hex.length);
